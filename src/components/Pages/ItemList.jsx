3ef2e3056a16499ffe93cab7c4921a12c8594239
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-07-11 22:09:41 -0300

    se acreo el boton de quitar

--- src/components/Pages/ItemList.jsx
+++ src/components/Pages/ItemList.jsx
@@ -1,24 +1,35 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 
 const ItemList = (props) => {
+    let stock = 10;
     const [contArticle, setArticle] = useState(0);
+
     const handleClick = () => {
-        setArticle(contArticle + 1);
+        if (contArticle < stock && contArticle >=0) {
+            setArticle(contArticle + 1);
+        }
     }
-  return (
-    <>
-        <section>
-            <article>
-                <h3>{props.title}</h3>
-                <p>{props.description}</p>
-            </article>
-            <article>
-                <h5>{props.price}</h5>
-                <button onClick={handleClick}>Agregar al carrito</button> {contArticle} Artículos
-            </article>
-        </section>
-    </>
-  )
+    const notHandleClick = () => {
+        if (contArticle <= stock && contArticle >0) {
+            setArticle(contArticle - 1);
+        }
+    }
+    return (
+        <>
+            <section>
+                <article>
+                    <h3>{props.title}</h3>
+                    <p>{props.description}</p>
+                </article>
+                <article>
+                    <h5>{props.price}</h5>
+                    <button onClick={handleClick}>Agregar al carrito</button>
+                    <p>{contArticle} Artículos</p>
+                    <button onClick={notHandleClick}>Quitar del carrito</button>
+                </article>
+            </section>
+        </>
+    )
 }
 
 export default ItemList
