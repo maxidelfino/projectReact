cbeb9f4c221ede1a8e1f787891a8c46701efec1d
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-07-28 23:44:12 -0300

    se agrego el boton volver para mejorar la navegabilidad

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const Cart = () => {
-  return (
-    <div>Cart</div>
-  )
+    return (
+        <>
+            <div>Cart</div>
+            <Link to={'/category'}><button className='countBtn'>Volver</button></Link>
+        </>
+    )
 }
 
 export default Cart
--- src/components/ItemDetail/ItemDetail.jsx
+++ src/components/ItemDetail/ItemDetail.jsx
@@ -1,27 +1,29 @@
 import React from 'react'
 import ItemCountLogic from '../ItemCount/ItemCountLogic';
+import { Link } from 'react-router-dom'
 
 const ItemDetail = ({ products }) => {
   return (
     <>
+      <Link to={'/category'}><button className='countBtn'>Volver</button></Link>
       <section className="itemContainer">
-            <article className="product-grid">
-                <div className="product-image">
-                    <img src={`${products.image}`} alt={products.title} />
-                </div>
-                <div className="product-content">
-                    <h3 className="title">{products.title}</h3>
-                </div>
-            </article>
-            <article className='product-description'>
-              <p>{products.description}</p>
-              <p className="price">${products.price}</p>
-              <div>
-              <ItemCountLogic initial={1} stock={10}/>
-              </div>
-              <p>Stock: 10</p>
-            </article>
-        </section>
+        <article className="product-grid">
+          <div className="product-image">
+            <img src={`${products.image}`} alt={products.title} />
+          </div>
+          <div className="product-content">
+            <h3 className="title">{products.title}</h3>
+          </div>
+        </article>
+        <article className='product-description'>
+          <p>{products.description}</p>
+          <p className="price">${products.price}</p>
+          <div>
+            <ItemCountLogic initial={1} stock={10} />
+          </div>
+          <p>Stock: 10</p>
+        </article>
+      </section>
     </>
   )
 }
