b737c667da618e27e04351b914350bbdb4242163
Maxi Delfino <maxidelfino1234@hotmail.com>
2022-08-02 11:24:07 -0300

    se le dio estilos al carrito

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,37 @@
-import React , {useContext} from 'react'
+import React, { useContext } from 'react'
 import { Link } from 'react-router-dom'
 import { CartContext } from '../../context/CartContext'
+import ItemCountLogic from '../ItemCount/ItemCountLogic'
 
 const Cart = () => {
     let test = useContext(CartContext)
-    console.log(test.cartList)
+    console.log(test.cartList) //no olvidemos que nuestro array del carrito es cartList!!!
     return (
         <>
             <div>Cart</div>
-            <hr />
             <Link to={'/category'}><button className='countBtn'>Volver</button></Link>
+            <hr />
+            <section className="itemCart">
+                {
+                    test.cartList.length > 0 && test.cartList.map(item => (
+                        <article className="product-cart">
+                            <div className="product-image">
+                                <img src={`${item.image}`} alt={item.title} />
+                            </div>
+                            <div className="product-content">
+                                <h3 className="title"><span>Producto: </span>{item.title}</h3>
+                            </div>
+                            <div className="product-price">
+                                <p className="price"><span>Precio: </span>${item.price}</p>
+                                <p className="quantity"><span>Cantidad: </span>2</p>
+                            </div>
+                            <div className="removeProducts">
+                                <ItemCountLogic/>
+                            </div>
+                        </article>
+                    ))
+                }
+            </section>
         </>
     )
 }
